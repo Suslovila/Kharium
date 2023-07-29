3113f1870c872697f068c1f1d9636f522c944358
JustCursed <mytojimbyylabu@gmail.com>
2023-07-29 23:31:29 +0300

    update

--- src/main/java/com/suslovila/ExampleMod.java
+++ src/main/java/com/suslovila/ExampleMod.java
@@ -2,26 +2,32 @@ package com.suslovila;
 
 import com.suslovila.client.ClientProxy;
 import com.suslovila.common.CommonProxy;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkRegistry;
-import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 
 @Mod(name = ExampleMod.NAME, modid = ExampleMod.MOD_ID, version = ExampleMod.VERSION, dependencies = "required-after:Thaumcraft")
-public class ExampleMod
-{
-	public static final SimpleNetworkWrapper NETWORK = new SimpleNetworkWrapper(ExampleMod.MOD_ID);
+public class ExampleMod {
 
 	public static final String NAME = "anticraft";
 	public static final String MOD_ID = "examplemod";
 	public static final String VERSION = "1.0";
 
+	public static final CreativeTabs ItemsMagicThings = new CreativeTabs("itemsMct") {
+        @Override
+        public Item getTabIconItem() {
+            return Item.getItemFromBlock(Blocks.water);
+        }
+    };
 
 	@Mod.Instance(MOD_ID)
 	public static ExampleMod instance;
